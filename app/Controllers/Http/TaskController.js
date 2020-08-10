234244ec87bcbf1a69342b95a533a14e019fa22a
Edvaldo Silva <edvaldodeveloper@gmail.com>
2020-08-09 23:07:19 -0300

    primeiro commit

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Task = use('App/Models/Task')
+
+class TaskController {
+   async index({ view }) {
+
+        const tasks = await Task.all()
+
+
+        return view.render('tasks', {
+            title: 'Latest tasks',
+            tasks: tasks.toJSON()
+        })
+    }
+}
+
+module.exports = TaskController
